db4026dc3649d7c3b95409a56e0f987e9b97312b
TempUsername2436 <c.benton.145@gmail.com>
2022-04-01 11:01:51 -0700

    Finished the basics and I have the game running

--- food.js
+++ food.js
@@ -1,13 +1,28 @@
-let food = { x: 0, y: 1};
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
+
+let food = getRandomFoodPosition()
+const EXPANSION_RATE = 5
 
 export function update() {
-
+  if (onSnake(food)) {
+    expandSnake(EXPANSION_RATE)
+    food = getRandomFoodPosition()
+  }
 }
 
 export function draw(gameBoard) {
-  const foodElment = document.createElement("div");
-  foodElment.style.gridRowStart = food.y;
-  foodElment.style.gridColumnStart = food.x;
-  foodElment.classList.add("food");
-  gameBoard.appendChild(foodElment);
+  const foodElement = document.createElement('div')
+  foodElement.style.gridRowStart = food.y
+  foodElement.style.gridColumnStart = food.x
+  foodElement.classList.add('food')
+  gameBoard.appendChild(foodElement)
+}
+
+function getRandomFoodPosition() {
+  let newFoodPosition
+  while (newFoodPosition == null || onSnake(newFoodPosition)) {
+    newFoodPosition = randomGridPosition()
+  }
+  return newFoodPosition
 }
