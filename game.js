d127b42e2ade4c91911e953e4887bfed1cd3c072
TempUsername2436 <c.benton.145@gmail.com>
2022-04-01 10:25:21 -0700

    Some of the rendering is done.

--- game.js
+++ game.js
@@ -1,10 +1,25 @@
+import { update as updateSnake, draw as drawSnake } from "./snake.js";
+import { SNAKE_SPEED } from "./snake.js";
+
 let lastRenderTime = 0;
+const gameBoard = document.getElementById("game-board");
 
 function main(currentTime) {
-  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
   window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
   lastRenderTime = currentTime;
-  console.log(currentTime);
+
+  update();
+  draw();
 }
 
 window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+}
+
+function draw() {
+  drawSnake(gameBoard);
+}
